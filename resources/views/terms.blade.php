c7434463b3958f3089a3e00fcd95df9b59a1a49f
Black River <anblackriver@gmail.com>
2024-04-03 21:41:19 +0500

    first commit1

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.layout')
+
+@section('content')
+    <h1>Terms</h1>
+    <div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores autem consequatur cumque deserunt enim error esse harum ipsam, iusto mollitia nobis officia, officiis perferendis porro, quibusdam recusandae repellat sed tenetur!</p>
+    </div>
+@endsection
